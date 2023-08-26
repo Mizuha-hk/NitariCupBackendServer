154b951a23026abca77704a13758a9045a330277
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-26 11:03:41 +0900

    [add]: model project

---
+++ NitariCupBackend.Models/TaskScheme.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitariCupBackend.Models
+{
+    public class TaskScheme
+    {
+        public Guid Id { get; set; }
+        public string userId { get; set; }
+        public string title { get; set; }
+        [AllowNull]
+        public string? description { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime limitDate { get; set; }
+        public DateTime createdAt { get; set; }
+        [AllowNull]
+        public bool isDone { get; set; } = false;
+        [AllowNull]
+        public DateTime? DoneDate { get; set; }
+        [AllowNull]
+        public float? score { get; set; }
+    }
+}
