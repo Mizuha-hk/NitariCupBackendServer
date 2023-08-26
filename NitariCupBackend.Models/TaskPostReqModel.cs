25d0147d879b9555c9f41c3c1e4dd5c1f28fa3c3
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-26 15:27:10 +0900

    [add]: Task endpoints

---
+++ NitariCupBackend.Models/TaskPostReqModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitariCupBackend.Models
+{
+    public class TaskPostReqModel
+    {
+        public string AccessToken { get; set; }
+        public string title { get; set; }
+        public string? description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime LimitDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
