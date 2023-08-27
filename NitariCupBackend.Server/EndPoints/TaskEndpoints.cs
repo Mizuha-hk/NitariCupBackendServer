95dba949dd2847fd2b47856498aaea841389dca6
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-08-27 14:18:39 +0900

    [fix]: notification not return response

--- NitariCupBackend.Server/EndPoints/TaskEndpoints.cs
+++ NitariCupBackend.Server/EndPoints/TaskEndpoints.cs
@@ -20,8 +20,11 @@ public static class TaskEndpoints
                 .OrderBy(model => model.startDate)
                 .Where(model => model.isDone == false)
                 .ToListAsync();
+            Task.WaitAll();
+
             var response = tasks
-                .Where(model => (model.startDate - now).Hours < 2 && (model.startDate - now).Hours > 0).ToList();
+                .Where(model => (model.startDate - now).Hours < 2 && (model.startDate - now).Hours > 0)
+                .ToList();
 
             return response == null
                 ? TypedResults.NotFound()
